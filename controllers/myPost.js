b37af92dd267b075c1afd46d41dedbe8661f2f3b
Phurinho <phurintoomkul@gmail.com>
2024-08-27 14:13:55 +0700

    Done

--- controllers/myPost.js
+++ controllers/myPost.js
@@ -3,7 +3,7 @@ const BlogPost = require('../models/BlogPost')
 module.exports = async(req,res) =>{
     const blogposts = await BlogPost.find({userid :req.session.userId}).populate('userid')
     if (req.session.userId){
-        return res.render('myPost',{
+        return res.render('mypost',{
             blogposts
         })
     }
